ba08a37fb73debd7b9610e298aeb40fc25bb6bbf
Alpinist282 <nurpiisov.nikita282@gmail.com>
2024-12-23 23:38:02 +0300

    fixed output for tests

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-class MyLineReg():
+class MyLineReg:
     def __init__(self, n_iter=100, learning_rate=0.1):
-        print(f"MyLineReg class: n_iter={n_iter}, learning_rate={learning_rate}")
+        self.n_iter = n_iter
+        self.learning_rate = learning_rate
+    
+    def __str__(self):
+        return f"MyLineReg class: n_iter={self.n_iter}, learning_rate={self.learning_rate}"
+    
